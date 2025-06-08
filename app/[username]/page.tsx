a9551cc48f0885c58b285ffa046b0212c8ee1f44
riddimabatham21 <riddimabatham21@gmail.com>
2025-06-08 22:37:22 +0530

    dynamic routing

---
+++ app/[username]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const channelPage = () => {
+    return (
+        //its called dynamic routing because when any route we seracrh in url if not found then the folder which is make lik [username] it will run
+        
+    <div>page</div>
+  )
+}
+
+export default channelPage
